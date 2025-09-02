18c324177a7b38aaf188b9c9ec759c53ecf5a4e3
linjiongwen <1219773367@qq.com>
2025-09-02 11:05:52 -0700

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold mb-4">汕头大学·香港中文大学联合汕头国际眼科中心</h1>
+      <p className="text-lg">近视防控 · 可视化平台</p>
+    </div>
+  );
+}
+
+export default App;
